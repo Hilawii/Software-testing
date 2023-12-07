7add2fb1c8130d90130179486b05baa8bcca32c7
Aleksi Karelius <aleksi.karelius@tuni.fi>
2023-12-07 14:22:52 +0200

    fix: more approriate assertion

--- src/compact_test.js
+++ src/compact_test.js
@@ -2,30 +2,30 @@ import compact from "./compact.js";
 import { expect } from 'chai';
 describe('compact', () => {
     it('removes falsey values from an array', () => {
-        expect(compact([0, 1, false, 2, '', 3])).equal([1, 2, 3]);
+        expect(compact([0, 1, false, 2, '', 3])).to.eql([1, 2, 3]);
     });
 
     it('returns an empty array when all values are falsey', () => {
-        expect(compact([0, false, '', null, undefined, NaN])).equal([]);
+        expect(compact([0, false, '', null, undefined, NaN])).to.eql([]);
     });
 
     it('handles an array with only truthy values', () => {
-        expect(compact([1, 2, 3])).equal([1, 2, 3]);
+        expect(compact([1, 2, 3])).to.eql([1, 2, 3]);
     });
 
     it('handles an empty array', () => {
-        expect(compact([])).equal([]);
+        expect(compact([])).to.eql([]);
     });
 
     it('handles arrays with complex elements', () => {
         const obj = { a: 1 };
         const func = () => {};
-        expect(compact([0, obj, '', func, NaN])).equal([obj, func]);
+        expect(compact([0, obj, '', func, NaN])).to.eql([obj, func]);
     });
 
     it('does not modify the original array', () => {
         const originalArray = [0, 1, false];
         compact(originalArray);
-        expect(originalArray).equal([0, 1, false]);
+        expect(originalArray).to.eql([0, 1, false]);
     });
 });
